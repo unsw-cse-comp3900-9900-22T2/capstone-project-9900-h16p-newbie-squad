d35bea12fabb7a64c1a0bdf290c6f1a63879b6c3
摸鱼Pro <74658051+Pro-Nou@users.noreply.github.com>
2022-07-13 23:23:03 +0800

    add input check & invisible passwords

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -3,12 +3,13 @@ import {Link, useNavigate } from "react-router-dom";
 import Header from './Header'
 import './login&signup.css'
 
-
+var password_visible = false
+var password = ''
 export default function LoginPage() {
+    password_visible = false
     const navigate = useNavigate()
     const Login = () => {
         var username = document.getElementById('User_name').value
-        var password = document.getElementById('password').value
         const data = {
             username: username,
             password: password,
@@ -45,21 +46,57 @@ export default function LoginPage() {
         <Header/>
         <div className='information-box'>
             <div className='container-noborder'>
-                <div className='login_page'>
+                <div className='all_center'>
                     <div>
-                        <div>User name</div>
+                        <div className='all_center'>User name</div>
                         <input className="inputBlock" type="text" id="User_name"/>
+                        <div className='all_center'>Password</div>
+                        <div>
+                            <input onChange={()=>PasswordVisible()} className="inputBlock" type="text" id="password"/>
+                            <button onClick={()=>ChangePasswordVisible()} id="password_visible">visible</button>
+                        </div>
+                        <div className='all_center'>
+                            <button onClick={()=>Login()}>Login</button>
+                        </div>
                     </div>
-                    <div>
-                        <div>Password</div>
-                        <input className="inputBlock" type="text" id="password"/>
-                    </div>
-                    <button onClick={() => Login()}>Login</button>
-                    <div>Do not have an account?</div>
-                    <Link to="/SignUp-Page"> <button >Sign up</button></Link>
                 </div>
             </div>
         </div>
     </div>
   )
+}
+function ChangePasswordVisible(){
+    password_visible=!password_visible
+    if(password_visible)
+    {
+        document.getElementById('password_visible').style.color = '#FFF'
+        document.getElementById('password_visible').style.backgroundColor = '#000'
+    }
+    else
+    {
+        document.getElementById('password_visible').style.color = '#000'
+        document.getElementById('password_visible').style.backgroundColor = '#FFF'
+    }
+    ShowPassword()
+}
+function PasswordVisible(){
+    var password_input = document.getElementById('password').value
+    if(password_input.length>password.length)
+        password+=password_input.substring(password.length)
+    else
+        password=password.substring(0,password_input.length)
+    //console.log(password)
+    ShowPassword()
+    //console.log('password')
+}
+function ShowPassword(){
+    if(password_visible)
+        document.getElementById('password').value = password
+    else 
+    {
+        var password_show=''
+        for(var i=0;i<password.length;i++)
+            password_show+='*'
+        document.getElementById('password').value = password_show
+    }
 }
