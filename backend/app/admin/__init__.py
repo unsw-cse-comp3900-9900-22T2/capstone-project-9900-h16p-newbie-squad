7d8c7a48076688b69c68c3c61ba69cd487d5e51a
z5335534 Ao Guo <z5335534@ad.unsw.edu.au>
2022-07-19 09:10:26 +0800

    init修改

--- backend/app/admin/__init__.py
+++ backend/app/admin/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
-from . import admin_views
+
 admin_bp = Blueprint('admin', __name__)
+from . import admin_views
 
 
--- backend/app/billing/__init__.py
+++ backend/app/billing/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 
-billing_bp = Blueprint('car', __name__)
+billing_bp = Blueprint('billing', __name__)
+# billing_bp = Blueprint('car', __name__)
 
 from . import billing_views
