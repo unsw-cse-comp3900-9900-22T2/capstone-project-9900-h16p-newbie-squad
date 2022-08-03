6d4f0f00d03585add9943839ecccd84ddd0d0fda
Yindi Ruan <z5333772@ad.unsw.edu.au>
2022-08-03 21:39:33 +1000

    almost

--- backend/app/models.py
+++ backend/app/models.py
@@ -9,7 +9,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     role_name = db.Column(db.String(64), unique=True)
-    #从Role到User一对多
+    #From Role to User: one to many relation
     users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
@@ -23,33 +23,25 @@ class User(db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     phone_num = db.Column(db.String(32), unique=True, index=True)
-    # bank_account = db.Column(db.String(32), unique=True, index=True)
-    # credit_card = db.Column(db.String(32), unique=True, index=True)
 
-    #前端将图片进行base64编码之后直接发送给后端，后端数据库存储该base64字节串
     avatar=db.Column(db.LargeBinary,nullable=True)
 
     bio=db.Column(db.String(128),nullable=True)
-    #外键链接到Role表，一对多，多的那一侧
+
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
-    #从user到vehicle表（一对多，一的那一侧）
+    #From users to vehicles: the one to many relation's "one" side
     vehicles = db.relationship('Vehicle', backref='owner', lazy='dynamic')
-    #从user到parking_space表（一对多，一的那一侧）
+    #From users to parking spaces: the one to many relation's "one" side
     parking_spaces = db.relationship('Parking_space', backref='owner', lazy='dynamic')
 
-    # 从user到request表（一对多，一的那一侧）
     requests = db.relationship('Request', backref='owner', lazy='dynamic')
 
-    # 从user到offer表（一对多，一的那一侧）
     offers = db.relationship('Offer', backref='owner', lazy='dynamic')
 
-    #从user到reviews表（一对多，一的那一侧）
     reviews = db.relationship('Review', backref='author', lazy='dynamic')
 
-    # 从user到bank_account表（一对一）
     bank_account = db.relationship('Bank_account', backref='owner', uselist=False)
-    # 从user 到 credit_card表（一对一）
     credit_card = db.relationship('Credit_card', backref='owner', uselist=False)
 
     customer=db.relationship('Booking',backref='customer',lazy='dynamic')
@@ -101,7 +93,6 @@ class Parking_space(db.Model):
     __tablename__ = 'parking_spaces'
     id = db.Column(db.Integer, primary_key=True)
 
-    #一对多，多的那一侧
     owner_id = db.Column(db.String(32), db.ForeignKey('users.id'))
 
     street = db.Column(db.String(32), nullable=False)
@@ -116,10 +107,9 @@ class Parking_space(db.Model):
     length = db.Column(db.Float, nullable=True)
     price = db.Column(db.Float, nullable=True)
 
-    #如果is_active为假，则代表此parking_space已经被用户删除
+    #if is_active is False，the parking_space is deleted
     is_active=db.Column(db.Boolean,nullable=False)
 
-    #一对多，一的那一侧
     bookings=db.relationship('Booking', backref='parking_space', lazy='dynamic')
     available_periods=db.relationship('Available_Period', backref='parking_space', lazy='dynamic',\
         cascade='all,delete-orphan')
@@ -138,7 +128,6 @@ class Parking_space(db.Model):
 class Available_Period(db.Model):
     __tablename__='available_periods'
     id = db.Column(db.Integer, primary_key=True)
-    #一对多，多的那一侧
     parking_space_id=db.Column(db.Integer, db.ForeignKey('parking_spaces.id'))
     start_date=db.Column(db.Date)
     end_date=db.Column(db.Date)
@@ -157,11 +146,9 @@ class Status:
 #The default
 #cascade behavior when an object is deleted is to set the foreign key in any related
 #objects that link to it to a null value.
-#Booking记录永远不会消失
 class Booking(db.Model):
     __tablename__='bookings'
     id = db.Column(db.Integer, primary_key=True)
-    #一对多，多的那一侧
     parking_space_id=db.Column(db.Integer,db.ForeignKey('parking_spaces.id'))
     customer_id=db.Column(db.Integer,db.ForeignKey('users.id'))
     status=db.Column(db.Integer)
@@ -182,7 +169,6 @@ class Review(db.Model):
     parking_space_id=db.Column(db.Integer,db.ForeignKey('parking_spaces.id'))
     booking_id=db.Column(db.Integer,db.ForeignKey('bookings.id'))
     review_text=db.Column(db.String(256), nullable=False)
-    #评分可以从1到5
     review_rating=db.Column(db.Integer, nullable=False)
     review_made_time=db.Column(db.DateTime,default=datetime.now)
 
@@ -190,20 +176,17 @@ class Review(db.Model):
         return '<reviews %r>' % self.id
 
 
-#所有的订单记录都保存在这张表中
+#all the billing histories are stored in this table
 class Billing(db.Model):
     __tablename__='billings'
     id = db.Column(db.Integer, primary_key=True,index=True)
 
-    #永久保存订单成功当时的provider和customer的id，便于后面搜索
     provider_id=db.Column(db.Integer,index=True)
     customer_id=db.Column(db.Integer,index=True)
 
-    #永久保存订单成功当时的provider和customer的username
     provider_name=db.Column(db.String(32))
     customer_name=db.Column(db.String(32))
 
-    #这张表只是存储历史记录，因此address不再分开了，生成历史记录的时候把street，suburb等合成一个字符串
     address=db.Column(db.String(64))
     start_date=db.Column(db.Date)
     end_date=db.Column(db.Date)
@@ -211,21 +194,14 @@ class Billing(db.Model):
     total_price=db.Column(db.Integer)
     payment_time=db.Column(db.DateTime,default=datetime.now)
 
-    #永久保存customer付款时用的银行卡号
     customer_card_number=db.Column(db.String(32))
 
-    #永久保存provider收款时用银行账户
     provider_bank_account=db.Column(db.String(32))
-
-    # #永久保存provider收款时用的银行卡号
-    # provider_card_number=db.Column(db.String(32))
 
 
 class Bank_account(db.Model):
     __tablename__ = 'bank_account'
-    # accoount_id, owner_id, account_name, bsb
     account_id = db.Column(db.String(32), primary_key=True)
-    # 一对多，多的那个
     owner_id = db.Column(db.String(32), db.ForeignKey('users.id'))
     account_name = db.Column(db.String(32))
     bsb = db.Column(db.String(32))
@@ -235,9 +211,7 @@ class Bank_account(db.Model):
 
 class Credit_card(db.Model):
     __tablename__ = 'credit_card'
-    # owner_id(db.ForeignKey('users.id')), card_number, card_name,Expiry_date, cvv
     card_number = db.Column(db.String(32), primary_key=True)
-    # 一对多，多的那个
     owner_id = db.Column(db.String(32), db.ForeignKey('users.id'))
     card_name = db.Column(db.String(32))
     expiry_date = db.Column(db.String(32))
@@ -250,7 +224,6 @@ class Credit_card(db.Model):
 class Request(db.Model):
     __tablename__ = 'requests'
     id = db.Column(db.Integer, primary_key=True)
-    # 一对多，多的那一侧
     owner_id = db.Column(db.String(32), db.ForeignKey('users.id'))
     title = db.Column(db.String(32), nullable=False)
     street = db.Column(db.String(32), nullable=False)
@@ -263,14 +236,10 @@ class Request(db.Model):
     start_date = db.Column(db.Date)
     end_date = db.Column(db.Date)
     others = db.Column(db.Text)
-    # 如果是complete == True，则表示该请求已经被处理
     complete = db.Column(db.Boolean, nullable=False)
-    # 如果is_active == False，则代表该请求已经被用户删除
     is_active = db.Column(db.Boolean,nullable=False)
-    # 如果publish == False，则代表该请求仅用户自己可见
     publish = db.Column(db.Boolean, nullable=False)
 
-    # 一对多，一的那一侧
     offers = db.relationship('Offer', backref='request', lazy='dynamic')
 
     def __repr__(self):
@@ -280,9 +249,7 @@ class Request(db.Model):
 class Offer(db.Model):
     __tablename__ = 'offers'
     id = db.Column(db.Integer, primary_key=True)
-    # 一对多，多的那一侧
     request_id = db.Column(db.Integer, db.ForeignKey('requests.id'))
-    # 一对多，多的那一侧
     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     street = db.Column(db.String(32), nullable=False)
     suburb = db.Column(db.String(32), nullable=False)
@@ -290,9 +257,7 @@ class Offer(db.Model):
     postcode = db.Column(db.Integer, nullable=False)
     price = db.Column(db.Float, nullable=True)
     comments = db.Column(db.Text)
-    # 如果is_active == False，则代表此offer已经被offer提供方删除
     is_active = db.Column(db.Boolean, nullable=False)
-    # 如果accept == True，则代表此offer已经被请求发起方接受
     accept = db.Column(db.Boolean, nullable=False)
 
     def __repr__(self):
